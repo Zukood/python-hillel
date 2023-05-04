27c8bfcc9e9b1845b52ad673022f379bdf942553
Zukood <74512169+Zukood@users.noreply.github.com>
2023-05-04 20:04:21 +0700

    Lesson_6, Task_4

---
+++ Lesson_6/Task_4.py
@@ -0,0 +1,13 @@
+lst = [23, 2, 3, 0]
+
+
+def change_list(lst):
+    if len(lst) < 2:
+        print("Cписок должен иметь не меньше 2 элементов")
+    else:
+
+        lst[0], lst[-1] = lst[-1], lst[0]
+        return lst
+
+
+print(change_list(lst))
