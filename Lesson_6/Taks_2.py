48c00e74f0133c09ff73fbf1f0662c54a7003c03
Zukood <74512169+Zukood@users.noreply.github.com>
2023-05-04 19:07:04 +0700

    Lesson_6, Task_2

--- Lesson_6/Taks_2.py
+++ Lesson_6/Taks_2.py
@@ -1,12 +1,10 @@
-import math
-
-
 def square(side):
     perimeter = 4 * side
     area = side ** 2
-    diagonal = side * math.sqrt(2)
+    diagonal = side * (2 ** 0.5)
     return perimeter, area, diagonal
 
 
 result = square(2)
-print(result)
+result_t = tuple(round(num, 2) for num in result)
+print("Периметр, площадь и диагональ квадрата: ", result_t)
---
+++ Lesson_6/Task_3.py
@@ -0,0 +1,6 @@
+
+
+
+
+def is_prime(a):
+
