c9d1c9d4d262f68f70649cbbd2f4cad717404f5d
Zukood <74512169+Zukood@users.noreply.github.com>
2023-05-04 21:01:17 +0700

    Lesson_6, Task_6

--- Lesson_6/task_6.py
+++ Lesson_6/task_6.py
@@ -1,5 +1,4 @@
 def sum_range(start, end):
-
     if start > end:
         start, end = end, start
 
@@ -8,7 +7,7 @@ def sum_range(start, end):
     for num in range(start, end + 1):
         total_sum += num
 
-
     return total_sum
+
 
 print(sum_range(102, 15))
