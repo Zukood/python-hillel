de2765fdef3f1e4d4da392f73ee2304f7bb6545b
Zukood <74512169+Zukood@users.noreply.github.com>
2023-05-12 20:14:20 +0700

    Lesson_9 task_1

---
+++ Lesson_9/task_1.py
@@ -0,0 +1,29 @@
+
+import json
+
+with open("group_people.json") as f:
+    json_file = json.load(f)
+
+max_group_id = ""  # Идентификатор группы с наибольшим количеством женщин после 1977 года
+max_female_count = 0  # Количество женщин после 1977 года в группе
+
+for group in json_file:
+    group_id = group["id_group"]
+    female_count = 0
+
+    for person in group["people"]:
+        if person["gender"] == "Female" and person["year"] > 1977:
+            female_count += 1
+
+    if female_count > max_female_count:
+        max_female_count = female_count
+        max_group_id = group_id
+
+print("Идентификатор группы:", max_group_id)
+print("Количество женщин после 1977 года:", max_female_count)
+
+
+
+
+
+
