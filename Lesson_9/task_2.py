b96b1b67c8df5a26e642972abe3bed25e53fcf47
Zukood <74512169+Zukood@users.noreply.github.com>
2023-05-16 22:28:59 +0700

    task_2

--- Lesson_9/task_2.py
+++ Lesson_9/task_2.py
@@ -1,11 +1,18 @@
 import json
 
-with open("manager_sales.json") as f:
-    json_file = json.load(f)
+with open('manager_sales.json') as file:
+    data = json.load(file)
 
-name = ""
-top_sales = 0
+max_sales = 0
+best_manager = ""
 
-for manager in json_file:
+for record in data:
+    manager = record["manager"]
+    sales = sum(car["price"] for car in record["cars"])
 
+    if sales > max_sales:
+        max_sales = sales
+        best_manager = manager
 
+result = f"{best_manager['first_name']} {best_manager['last_name']}: {max_sales}"
+print(result)
